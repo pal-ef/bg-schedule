06fc713ee38d3d6750cfed1aa5630280428efb20
Yin <soft.pal.ef@gmail.com>
2022-10-26 23:34:16 -0500

    alpha version

---
+++ conf.sh
@@ -0,0 +1,27 @@
+FONT="./inter/Inter-Light.otf"
+CANVAS_SIZE="1920x1080"
+CANVAS_COLOR="#000000"
+
+OUTPUT_FILENAME="output.png"
+INPUT_FILENAME="input.png"
+
+LOGO=$(find "./img" -type f | shuf -n 1)
+
+TITLE="DSchedule"
+TITLE_FS=30
+NORMAL_FS=25
+
+# Schedule
+T1=("Recite" "the" "national" "anthem")
+T2="Write a poem about lettuces"
+T3="Watch anime ilegally"
+T4="Practice bird singing"
+T5="Math for noobs"
+T6="Take a shit"
+T7="Eat something"
+T8="Drink some water"
+T9="Be like water"
+TA="Enjoy the landscape"
+TB="Fight with dwarfs about socialism"
+TC="Write a hateful asd"
+TD="Write something"
--- custombg.sh
+++ custombg.sh
@@ -1,26 +1,77 @@
 #! /bin/bash
+source ./conf.sh
+if [[ `gsettings get org.gnome.desktop.interface color-scheme` =~ 'dark' ]];
+then
+    DARK_MODE=true
+    TXT_COLOR="#FFFFFF"
+    CANVAS_COLOR="black"
+else
+    DARK_MODE=false
+    TXT_COLOR="#000000"
+    CANVAS_COLOR="white"
+fi
 
-#convert test.png -gravity North -pointsize 60 -annotate +0+100 'test' converted.png
+TIME=$(date +%T)
+TIME=${TIME:0:2}
+
+case $TIME in
+    21)
+        OPTION="#F2323F"
+        ;;
+    22)
+        echo "son las 10"
+        S9=false
+        ;;
+    *)
+        echo "FAIL"
+        ;;
+esac
+
+# CREATE A BLANK IMAGE
+convert \
+    -size $CANVAS_SIZE \
+    canvas:$CANVAS_COLOR \
+    $OUTPUT_FILENAME
+    
+convert $OUTPUT_FILENAME \
+    \( $LOGO -thumbnail x1080 \) -gravity east -composite \
+    $OUTPUT_FILENAME
+
+distance=450
+
+convert $OUTPUT_FILENAME \
+    -font $FONT \
+    -gravity west \
+    -pointsize $NORMAL_FS \
+    -fill $TXT_COLOR \
+    $(
+        for i in 09 10 11 12 13 14 15 16 17 18 19 20 21
+        do
+            echo " -annotate +50-$distance $i:00 "
+            ((distance=distance-75))
+        done
+    ) \
+    $(
+        ((distance=450))
+        for i in {1..3}
+        do
+
+            echo " -annotate +200-$distance "${T1[@]}" "
+            ((distance=distance-75))
+        done
+    ) \
+    -fill "rgb(217,217,217)" \
+    -draw 'rectangle 155,1030 165,70' \
+    converted.png
+
+pic="${HOME}/Code/scripts/converted.png"
 
 
-# Draw a blue box
-#convert test.png -strokewidth 0 -fill "rgba(49,49,238,0.8)" -draw "rectangle 600,50 50,150" converted.png 
+gsettings set org.gnome.desktop.background picture-options 'scaled'
 
-# Draw some text
-#convert -font /usr/share/fonts/urw-base35/NimbusMonoPS-BoldItalic.otf -fill white -pointsize 60 -draw 'text 100,100 "Schedule 1.0"' test.png converted.png
-
-FONT="/usr/share/fonts/adobe-source-code-pro/SourceCodePro-Regular.otf"
-SIZE="600x50"
-BACKGROUND="black"
-
-convert test.png \
-    \( \
-        -size $SIZE \
-        -background $BACKGROUND \
-        -fill white \
-        -font $FONT \
-        label:"Example" \
-        -trim \
-        -gravity center \
-        -extent 600x100 \
-    \) -gravity northwest -geometry +312+66 -composite converted.png
+if $DARK_MODE;
+then
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$pic"
+else
+    gsettings set org.gnome.desktop.background picture-uri "file://$pic"
+fi
